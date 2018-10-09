132db39bc536f07316b69d58757888052ef97a2d
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-10-09 12:00:38 -0300

    mudando webpack para a versão 3 e criando o index de base

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist')
-	}
-  };
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        publicPath: '/dist',
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new ExtractTextPlugin({
+            filename: 'styles.css'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+};
